e54b98766990b4137d5bb61b3c59a0482af27039
akukhta <electricsix23@.com>
2023-12-19 19:25:36 +0100

    tests refactoring

---
+++ test/reader/MMapReaderTest/onePageRead/main.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MODULE Test1
+#include "../../../../src/reader/MMapFileReader.hpp"
+#include "../../../Utiles/TmpFile.hpp"
+#include <boost/test/included/unit_test.hpp>
+#include <chrono>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+
+BOOST_AUTO_TEST_CASE(OnePageRead)
+{
+    auto tmpFile = TmpFile::create();
+
+	auto onePageBuf = std::vector<unsigned char>(tmpFile.content.data(), tmpFile.content.data() + MMapFileReader::defaultBufferSize);
+    
+    try
+    {
+        MMapFileReader reader(tmpFile.fileName);
+        reader.open();
+
+        auto b = reader.read();
+
+        BOOST_ASSERT(b == onePageBuf);
+    }
+    catch(std::runtime_error const& err)
+    {
+        std::cout << err.what() << std::endl;
+        BOOST_ASSERT(false);
+    }
+}
