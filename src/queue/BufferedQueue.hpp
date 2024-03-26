019508a55cf97556d65ee4c975ff7ba4892d8335
akukhta <electricsix23@.com>
2024-03-26 14:11:54 +0100

    Buffered Queue supports IPC and NonIPC Base

--- src/queue/BufferedQueue.hpp
+++ src/queue/BufferedQueue.hpp
@@ -8,16 +8,33 @@
 #include <array>
 #include <mutex>
 #include <deque>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
 
 #define BUFFERS_IN_QUEUE 500
 
+struct NonIPCBase
+{
+    std::array<std::array<unsigned char, BUFFER_SIZE>, BUFFERS_IN_QUEUE> buffers{};
+};
+
+struct IPCBase
+{
+
+};
+
+template <class T>
+concept IsNonIPC = std::derived_from<T, NonIPCBase>;
+
 template <class MutexType, class ConditionType, template<class> class RAIILockType, template<class> class DequeType>
-class FixedBufferQueue
+class FixedBufferQueue : public std::conditional_t<std::is_same_v<MutexType, std::mutex>, NonIPCBase, IPCBase>
 {
 public:
+
+    template<typename T = FixedBufferQueue<MutexType, ConditionType, RAIILockType, DequeType>>
+        requires IsNonIPC<T>
     FixedBufferQueue()
     {
-        for (auto & buffer : buffers)
+        for (auto &buffer : NonIPCBase::buffers)
         {
             readBuffers.emplace_back(readBuffers, writeBuffers, cv, queueMutex, buffer.data());
         }
@@ -89,7 +106,6 @@ public:
 
 private:    
     DequeType<Buffer> readBuffers, writeBuffers;
-    std::array<std::array<unsigned char, BUFFER_SIZE>, BUFFERS_IN_QUEUE> buffers{};
     mutable MutexType queueMutex;
     ConditionType cv;
     std::atomic_bool isOpen{false};
