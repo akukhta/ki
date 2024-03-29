a726c9c1554133a8ce3878599e507c571d367111
akukhta <electricsix23@.com>
2024-03-29 11:09:57 +0100

    Remaining cpp files removed

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-conan install . --output-folder=bin 
-cmake . -B bin --preset conan-release
-cmake --build bin
+conan install . --output-folder=bin --profile=../debugprofile.txt --profile:b=../debugprofile.txt --build=missing
+cmake . -B bin -DCMAKE_BUILD_TYPE=Debug --preset conan-debug
+cmake --build bin --config debug
 cd bin
 ctest
