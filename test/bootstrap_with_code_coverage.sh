cbd65a79fd8812efbb90f2af6faf2a1824e700f0
akukhta <electricsix23@.com>
2024-03-29 15:02:01 +0100

    Code Coverage info is now being added as a commit comment, code coverage local added

---
+++ test/bootstrap_with_code_coverage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+conan install . --output-folder=bin --profile=../debugprofile.txt --profile:b=../debugprofile.txt --build=missing
+cmake . -B bin -DCMAKE_BUILD_TYPE=Debug --preset conan-debug
+cmake --build bin --config debug
+cd bin
+ctest
+
+cd ../
+mkdir coverage
+cd ..
+
+gcovr --gcov-executable gcov-13 . --json-summary --json ./test/coverage/coverage-summary.json --json-summary-pretty --cobertura ./test/coverage/coverage.cobertura.xml --cobertura-pretty --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
+lcov -c -d . -o ./test/coverage/my.info --gcov-tool gcov-13 --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
+genhtml -o ./test/coverage/ ./test/coverage/my.info
