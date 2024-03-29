38039a7d1fb775279028585e803448cbbd893917
akukhta <electricsix23@.com>
2024-03-29 15:15:16 +0100

    bootstrap_gh_actions.sh update

--- test/bootstrap_gh_actions.sh
+++ test/bootstrap_gh_actions.sh
@@ -10,6 +10,4 @@ cd ../
 mkdir coverage
 cd ..
 
-gcovr --gcov-executable gcov-13 . --json-summary --json ./test/coverage/coverage-summary.json --json-summary-pretty --cobertura ./test/coverage/coverage.cobertura.xml --cobertura-pretty --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
-#lcov -c -d . -o ./test/coverage/my.info --gcov-tool gcov-13 --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
-#genhtml -o ./test/coverage/ ./test/coverage/my.info
+gcovr --gcov-executable gcov-13 . --json-summary --json ./test/coverage/coverage-summary.json --json-summary-pretty --cobertura ./test/coverage/coverage.cobertura.xml --cobertura-pretty --exclude "13/*" --exclude "/usr/include/*" --exclude "test/*"
