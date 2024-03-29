57bb43707ff9a198d234c6be914f21a3907dd1f1
Alexey Kuhta <electricsix23@gmail.com>
2024-03-29 13:15:18 +0100

    Update bootstrap.sh

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -6,7 +6,10 @@ cmake --build bin --config debug
 cd bin
 ctest
 
-cd ../../
+cd ../
+mkdir coverage
+cd ..
+
 gcovr --gcov-executable gcov-13 . --json-summary --json-summary-pretty --cobertura ./test/coverage/coverage.cobertura.xml --cobertura-pretty
 lcov -c -d . -o ./test/coverage/my.info --gcov-tool gcov-13 --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
 genhtml -o ./test/coverage/ ./test/coverage/my.info
