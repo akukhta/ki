6c27de0645169f3110966ce5dcf0f8fa69cbc141
akukhta <electricsix23@.com>
2024-03-29 12:47:01 +0100

    Code Coverage

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -5,3 +5,8 @@ cmake . -B bin -DCMAKE_BUILD_TYPE=Debug --preset conan-debug
 cmake --build bin --config debug
 cd bin
 ctest
+
+cd ../../
+gcovr --gcov-executable gcov-13 . --json-summary --json-summary-pretty --cobertura ./test/coverage/coverage.cobertura.xml --cobertura-pretty
+lcov -c -d . -o ./test/coverage/my.info --gcov-tool gcov-13 --exclude "*/boost/*" --exclude "13/*" --exclude "/usr/include/*" --exclude "*/test/*"
+genhtml -o ./test/coverage/ ./test/coverage/my.info
